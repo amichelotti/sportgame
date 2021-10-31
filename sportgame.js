af5b1d8de449f4cee8e42564eee995902be85415
Alessandro Michelotti <90627984+michelottialessandro@users.noreply.github.com>
2021-10-31 19:41:27 +0100

    update

--- sportgame.js
+++ sportgame.js
@@ -5,8 +5,8 @@ server.listen(3001);
 const wsServer = new WebSocketServer({
     httpServer: server
 });
-console.log("startimg server on 3001");
-var players={};
+console.log("starting server on 3001");
+var players={}; //players info
 function listOfPlayers(){
     var l=[];
     for(var k in players){
@@ -20,29 +20,35 @@ wsServer.on('request', function(request) {
     connection.on('message', function(message) {
       console.log('Received Message:', message.utf8Data);
       try{
-        var data=JSON.parse( message.utf8Data);
-        if(data.hasOwnProperty("device")&&(data.hasOnwPropert("cmd"))){
+        var data=JSON.parse( message.utf8Data);//DECODE STRING INTO JSON OBJECT
+        if(data.hasOwnProperty("device")&&(data.hasOwnProperty("cmd"))){
             // i pacchetti dal client devono avere sempre il deviceid (il telefono) ed il comando da fare
             if(data['cmd']=="request_to_join"){
                 players[data['device']]=data; // infila nella mappa device a cui e' associato il player
                 console.log("added:"+data['player']);
                 data['player_list']=listOfPlayers();
                 data['cmd']="player_list";
-                connection.sendUTF(data); // return the list of current players
+                var d={
+                    cmd:"player_list",
+                    player_list:listOfPlayers()
+
+                }
+                players['connection']=connection; // save the connection for further communications
+                //answer with the current player list 
+                connection.sendUTF(JSON.stringify(d)); // return the list of current players, must be sent as string so JSON.stringify
             } else if(data['cmd']=="player_list"){
                 var d={
                     'cmd':"player_list",
                     'player_list':listOfPlayers()
                 }
-                connection.sendUTF(d); // return the list of current players
+                connection.sendUTF(JSON.stringify(d)); // return the list of current players
 
             }
 
         }
       } catch(e){
-          console.log("## cannot parse JSON ");
+          console.log("## error :"+e);
       }
-      connection.sendUTF(message.utf8Data+' Hi this is WebSocket server!');
     });
     connection.on('close', function(reasonCode, description) {
         console.log('Client has disconnected.');
